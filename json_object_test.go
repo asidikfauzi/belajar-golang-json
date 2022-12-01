9e344d0021d6cb2998354fc37d3be96c333e4375
asidikfauzi <asidikfauzi@gmail.com>
2022-12-01 08:11:29 +0700

    json object

---
+++ json_object_test.go
@@ -0,0 +1,28 @@
+package belajar_golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Customer struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+	Age        int
+	Married    bool
+}
+
+func TestJsonObject(t *testing.T) {
+	customer := Customer{
+		FirstName:  "Achmad",
+		MiddleName: "Sidik",
+		LastName:   "Fauzi",
+		Age:        23,
+		Married:    false,
+	}
+
+	bytes, _ := json.Marshal(customer)
+	fmt.Println(string(bytes))
+}
